045081e7d122116c7efd895d9f59216728a96ed6
kochiyama <marcelokochiyama2210@gmail.com>
2021-02-26 16:46:29 -0400

    create file upload method

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import multerConfig from './config/multer';
+
+import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 
 const router = new Router();
 
+const upload = multer(multerConfig);
+
 router.get('/users', UserController.show);
 router.post('/users', UserController.create);
+router.post('/files', upload.single('file'), FileController.create);
 
 export default router;
