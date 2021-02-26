3794668da4dd015a3ba41c6512c03210c8cf20b7
kochiyama <marcelokochiyama2210@gmail.com>
2021-02-26 16:38:21 -0400

    create route to show all users (temporary)

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 
 const router = new Router();
 
+router.get('/users', UserController.show);
 router.post('/users', UserController.create);
 
 export default router;
