d55f4935dea5850e00acb258b88310ce6261d740
kochiyama <marcelokochiyama2210@gmail.com>
2021-02-26 16:28:39 -0400

    update: add data validation with yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,18 @@
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class UserController {
 	async create(req, res) {
+		const schema = Yup.object().shape({
+			email: Yup.string().email().required(),
+			name: Yup.string().required(),
+			password: Yup.string().required().min(6),
+		});
+
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ error: 'Data validation failed.' });
+		}
+
 		const emailAlreadyRegistered = await User.findOne({
 			where: { email: req.body.email },
 		});
